0df69cd14606b7e5326c9dede4fed546327332bb
Harmanjit Randhawa <harmanjit.randhawa98@gmail.com>
2024-08-17 12:53:59 -0600

    updated useEffect to use axios instead of fetch

--- React/basics/blog-app/src/components/Home/Home.js
+++ React/basics/blog-app/src/components/Home/Home.js
@@ -1,27 +1,42 @@
-import React, {useState, useEffect} from 'react'
-import { BlogList } from '../BlogList/BlogList'
+import React, {useState, useEffect} from 'react';
+import { BlogList } from '../BlogList/BlogList';
+import axios from 'axios';
+
+
 export default function Home() {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    fetch(`http://localhost:8000/blogs`)
-    .then(res => {
-      if(!res.ok){
-        console.log('Get request failed');
-        throw new Error("Get request failed to json server for blogs");
-      }
-      return res.json();
-    })
-    .then((json) => {
-      setTimeout(()=>{
-        setBlogs(json);
-        setIsLoading(false);
-      }, 1000);
+  // useEffect(() => {
+  //   fetch(`http://localhost:8000/blogs`)
+  //   .then(res => {
+  //     if(!res.ok){
+  //       console.log('Get request failed');
+  //       throw new Error("Get request failed to json server for blogs");
+  //     }
+  //     return res.json();
+  //   })
+  //   .then((json) => {
+  //     setTimeout(()=>{
+  //       setBlogs(json);
+  //       setIsLoading(false);
+  //     }, 1000);
 
+  //   })
+  // }, []);
+
+  useEffect(()=>{
+    axios.get('http://localhost:8000/blogs')
+    .then(res => {
+      setTimeout(()=>{
+        setBlogs(res.data);
+        setIsLoading(false);
+      },1500)
     })
-  }, []);
+    .catch(err => console.error(err));
+  },[]);
+
 
   return (
     <div className='home'>
