893bd7191943843c7c6db69d233dc6d6898584be
Harmanjit Randhawa <harmanjit.randhawa98@gmail.com>
2024-08-24 12:54:40 -0600

    Created a new form for post requests

--- React/basics/blog-app/src/components/Home/Home.js
+++ React/basics/blog-app/src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import { BlogList } from '../BlogList/BlogList';
 import useFetch from '../../hooks/useFetch';
 
--- React/basics/blog-app/src/components/NewBlogForm/NewBlogForm.js
+++ React/basics/blog-app/src/components/NewBlogForm/NewBlogForm.js
@@ -1,10 +1,38 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './NewBlogForm.css'
 export default function NewBlogForm() {
 
+
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Mario');
   return (
     <div className="create">
       <h2>Add a new Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input type="text" required 
+          value={title} 
+          onChange={(e) => setTitle(e.target.value)} />
+
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+
+        <label>Blog author</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+
+        <button>Add Blog!!!</button>
+      </form>
     </div>
   )
 }
