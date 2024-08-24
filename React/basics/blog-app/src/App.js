30b6c712e45cca17239b776d96d21cfe9e6cff11
Harmanjit Randhawa <harmanjit.randhawa98@gmail.com>
2024-08-24 13:41:43 -0600

    Added 404 component and routing

--- React/basics/blog-app/src/App.js
+++ React/basics/blog-app/src/App.js
@@ -3,6 +3,7 @@ import Home from './components/Home/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NewBlogForm from './components/NewBlogForm/NewBlogForm';
 import Blog from './components/Blog/Blog';
+import NotFound from './components/NotFound/NotFound';
 
 
 function App() {
@@ -15,6 +16,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/addNewBlog" element={<NewBlogForm />} />
             <Route path="/blogs/:id" element={<Blog />} />
+            
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
 
---
+++ React/basics/blog-app/src/components/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NotFound() {
+
+  return (
+    <div className='not-found'>
+      <h2>Sorry.....</h2>
+      <p>That page cannot be found</p>
+      <Link to='/'>Back to home page</Link>
+    </div>
+  )
+}
